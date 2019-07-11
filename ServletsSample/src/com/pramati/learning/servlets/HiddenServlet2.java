811bed67e13fb481eac51af2405c7824d731de33
AlimiSandeep <alimisandeep97@gmail.com>
2019-07-11 17:37:39 +0530

    removing servers from git

---
+++ ServletsSample/src/com/pramati/learning/servlets/HiddenServlet2.java
@@ -0,0 +1,20 @@
+package com.pramati.learning.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HiddenServlet2 extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		PrintWriter pw = resp.getWriter();
+
+		String name = req.getParameter("user");
+		pw.println("<h3>Good Morning...." + name + "</h3>");
+	}
+}
